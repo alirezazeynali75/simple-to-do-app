2b6ac0ac8bf528d99273b9586a01fd65c31ffe96
alirezazeynali <alirezazeynali75@gmail.com>
2022-07-24 15:22:38 +0430

    Add add base service

--- domain/services/user_service.go
+++ domain/services/user_service.go
@@ -1,5 +1,11 @@
 package services
 
+import (
+	"github.com/alirezazeynali75/simple-to-do-app/domain"
+	"github.com/alirezazeynali75/simple-to-do-app/domain/ports"
+)
+
 type UserService struct {
-	
+	userRepo ports.UserRepository
+	jwt *domain.JwtManager
 }
